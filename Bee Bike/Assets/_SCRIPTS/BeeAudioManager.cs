8e97dea7e30181ef2711c0d3a6417ca24db5871e
Matthew Napolillo <mnapolillo42@gmail.com>
2019-05-23 01:22:44 -0400

    Adjusted grass height. Added Bee Collision Event Handler to the player bee object. Added Bee Audio Manager to the environment bees prefab. Together these will handle the activation of bee audio when obtaining a bee follower.

---
+++ Bee Bike/Assets/_SCRIPTS/BeeAudioManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeeAudioManager : MonoBehaviour
+{
+    
+    public AudioSource audioSource;
+    public float volumeChangeSpeed = 0.5f;
+
+    private Coroutine lowerRoutine;
+    private Coroutine raiseRoutine;
+
+    private IEnumerator LowerAudioSource()
+    {
+        float startVol = 1f;
+        while(startVol > 0)
+        {
+            startVol += Time.deltaTime * volumeChangeSpeed;
+            audioSource.volume = Mathf.Clamp01(startVol);
+            yield return null;
+        }
+    }
+
+    private IEnumerator RaiseAudioSource()
+    {
+        float startVol = 0f;
+        while(startVol < 1)
+        {
+            startVol -= Time.deltaTime * volumeChangeSpeed;
+            audioSource.volume = Mathf.Clamp01(startVol);
+            yield return null;
+        }
+    }
+
+    public void ActivateAudioSource()
+    {
+        StartCoroutine(RaiseAudioSource());
+    }
+
+    public void DectivateAudioSource()
+    {
+        StartCoroutine(LowerAudioSource());
+    }
+
+}
