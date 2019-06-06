46c4a6b7d36df0e856a6a828536c02ecf4cfc095
Steven Lilley <35742090+lilleypad13@users.noreply.github.com>
2019-06-06 03:35:53 -0400

    Updated GPU_Instancing script to handle spawning flower meshes in designated positions and giving them movement; Created a prefab object that holds this script (GPU_BatchRenderer) to be added to the BeeIsland scene. SpawningFormation script needs to be added to a child object of the player controller

---
+++ Bee Bike/Assets/_SCRIPTS/Following/SpawningFormation.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawningFormation : MonoBehaviour
+{
+    #region Variables
+
+    private static Transform[] spawningPositions;
+    private static int formationCounter = -1; // Being initialized at -1 since we increment it before its first use for finding an array element
+
+    #endregion
+
+    #region Unity Methods
+
+    /*
+     * Initializes the array holding the spawningPositions by creating an array with a number of elements directly 
+     * equal to the number of children this gameObject has. This ensures the array is the proper size to hold 
+     * each child gameObject individually.
+     */
+
+    private void Start()
+    {
+        spawningPositions = new Transform[transform.childCount];
+
+        //Debug.Log("Number of Children: " + transform.childCount);
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            spawningPositions[i] = transform.GetChild(i).gameObject.transform;
+        }
+
+        GPU_Instancing.setInstantiationArray(spawningPositions);
+    }
+
+    #endregion
+
+}
--- Bee Bike/Assets/_SCRIPTS/GPU_Instancing.cs
+++ Bee Bike/Assets/_SCRIPTS/GPU_Instancing.cs
@@ -29,57 +29,104 @@ public class ObjData
 
 public class GPU_Instancing : MonoBehaviour
 {
-    public int instances;
-    public Vector3 maxPos;
-    public Mesh objMesh;
-    public Material objMaterial;
+    //public int instances;
+    //public Vector3 maxPos;
 
+    [Header("Mesh Rendering Factors")]
+    public Mesh _objMesh;
+    public static Mesh objMesh;
+    public Material objMaterial;
     public Vector3 instantiationScale = new Vector3(1.0f, 1.0f, 1.0f);
 
     private List<List<ObjData>> batches = new List<List<ObjData>>();
+    private static bool isRunning = true;
+    private static Transform[] spawningLocations;
+    //public Transform[] spawningLocations;
+    //public Transform instantiateLocation;
+    //public int flowersToSpawnAtOnce = 3;
+
+    [Header("Flower Spawn and Movement Variables")]
+    public float timeBetweenInstantiations = 1.0f;
+    public float testMoveSpeed = 0.01f;
+    public float initialHeight = 0.0f;
+    public float finalHeight = 20.0f;
+
+    // Noise Range Variables
+    [Header("Noise for Spawn Locations")]
+    public float xNoise = 1.0f;
+    public float yNoise = 1.0f;
+    public float zNoise = 1.0f;
+
+
 
     private void Start()
     {
-        int batchIndexNum = 0;
+        setObjectMesh(_objMesh);
+
+        //int batchIndexNum = 0;
         List<ObjData> currBatch = new List<ObjData>();
 
-        for (int i = 0; i < instances; i++)
-        {
-            AddObj(currBatch, i);
-            batchIndexNum++;
-            if(batchIndexNum >= 1000)
-            {
-                batches.Add(currBatch);
-                currBatch = BuildNewBatch();
-                batchIndexNum = 0;
-            }
-        }
+        batches.Add(currBatch);
 
-        if(batchIndexNum != 0)
-        {
-            batches.Add(currBatch);
-            batchIndexNum = 0;
-        }
+        //for (int i = 0; i < instances; i++)
+        //{
+        //    AddObj(currBatch, i);
+        //    batchIndexNum++;
+        //    if (batchIndexNum >= 1000)
+        //    {
+        //        batches.Add(currBatch);
+        //        currBatch = BuildNewBatch();
+        //        batchIndexNum = 0;
+        //    }
+        //}
+
+        //if (batchIndexNum != 0)
+        //{
+        //    batches.Add(currBatch);
+        //    batchIndexNum = 0;
+        //}
+
+
     }
 
     private void Update()
     {
         RenderBatches();
+        MoveObjects(batches[0]);
+        //CreateNewObject(batches[0]);
+        if (isRunning == false)
+        {
+            StartCoroutine(TimedObjectCreation(batches[0]));
+        }
     }
 
     private void RenderBatches()
     {
-        foreach(var batch in batches)
+        foreach (var batch in batches)
         {
-            Graphics.DrawMeshInstanced(objMesh, 0, objMaterial, batch.Select((a) => a.matrix).ToList());
+            Graphics.DrawMeshInstanced(objMesh, 0, objMaterial, batch.Select(a => a.matrix).ToList());
+            /* a => a.matrix:
+             * void FuncName (a){
+             *      a.matrix;
+             * }
+             * 
+             * batch.Select(a => a.matrix).ToList()
+             * satisfies the parameter type:
+             * List<Matrix4x4> matrices
+             * 
+             * batches is a list of lists of ObjData objects
+             * so batch is a single list of ObjData objects
+             * batch.Select is "selecting" the matrix property of the ObjData a
+             * this is then added to a list
+             */
         }
     }
 
-    private void AddObj(List<ObjData> currBatch, int i)
-    {
-        Vector3 position = new Vector3(Random.Range(-maxPos.x, maxPos.x), Random.Range(-maxPos.y, maxPos.y), Random.Range(-maxPos.z, maxPos.z));
-        currBatch.Add(new ObjData(position, new Vector3(instantiationScale.x, instantiationScale.y, instantiationScale.z), Quaternion.identity));
-    }
+    //private void AddObj(List<ObjData> currBatch, int i)
+    //{
+    //    Vector3 position = new Vector3(Random.Range(-maxPos.x, maxPos.x), Random.Range(-maxPos.y, maxPos.y), Random.Range(-maxPos.z, maxPos.z));
+    //    currBatch.Add(new ObjData(position, new Vector3(instantiationScale.x, instantiationScale.y, instantiationScale.z), Quaternion.identity));
+    //}
 
     private List<ObjData> BuildNewBatch()
     {
@@ -87,4 +134,104 @@ public class GPU_Instancing : MonoBehaviour
     }
 
 
+    /*
+     * Takes an entire list of ObjData objects and moves them in the y direction according to the testMoveSpeed
+     */
+    private void MoveObjects(List<ObjData> batchToMove)
+    {
+        foreach (var thingy in batchToMove)
+        {
+            if (thingy.pos.y < finalHeight)
+            {
+                thingy.pos.y += testMoveSpeed;
+            }
+        }
+    }
+
+
+    /* 
+     * Used to add another object to a list of ObjData at a specified location
+     */
+    //private void CreateNewObject(List<ObjData> currBatch)
+    //{
+    //    Vector3 position = instantiateLocation.transform.position;
+    //    position = AddNoise(position);
+    //    currBatch.Add(new ObjData(position, new Vector3(instantiationScale.x, instantiationScale.y, instantiationScale.z), Quaternion.identity));
+    //}
+
+
+    /* 
+     * Used to add multiple objects to a list of ObjData at a specified location at a time
+     */
+    //private void CreateMultipleNewObjects(List<ObjData> currBatch)
+    //{
+    //    for (int i = 0; i < flowersToSpawnAtOnce; i++)
+    //    {
+    //        Vector3 position = instantiateLocation.transform.position;
+    //        position = AddNoise(position);
+    //        currBatch.Add(new ObjData(position, new Vector3(instantiationScale.x, instantiationScale.y, instantiationScale.z), Quaternion.identity));
+    //    }
+    //}
+
+
+    /*
+     * Allows for setting of the spawningLocations array
+     */
+    public static void setInstantiationArray(Transform[] spawningFormation)
+    {
+        spawningLocations = spawningFormation;
+
+        isRunning = false;
+    }
+
+    /*
+     * Sets the objMesh to a new mesh so future renders will be a new mesh
+     */
+    public static void setObjectMesh(Mesh meshToSet)
+    {
+        objMesh = meshToSet;
+
+        Debug.Log("objMesh set to: " + objMesh.name);
+    }
+
+    /* 
+     * Used to add multiple objects to a list of ObjData at a specified location at a time using an 
+     * array of locations to generally position the objects
+     */
+    private void CreateMultipleNewObjectsFormation(List<ObjData> currBatch)
+    {
+        for (int i = 0; i < spawningLocations.Length; i++)
+        {
+            Vector3 position = spawningLocations[i].transform.position;
+            position = AddNoise(position);
+
+            position.y = initialHeight;
+            currBatch.Add(new ObjData(position, new Vector3(instantiationScale.x, instantiationScale.y, instantiationScale.z), Quaternion.identity));
+        }
+    }
+
+
+    /* 
+     * Allows for timed use of the CreateNewObject method
+     */
+    private IEnumerator TimedObjectCreation(List<ObjData> currBatch)
+    {
+        isRunning = true;
+
+        yield return new WaitForSeconds(timeBetweenInstantiations);
+        //CreateNewObject(currBatch);
+        //CreateMultipleNewObjects(currBatch);
+        CreateMultipleNewObjectsFormation(currBatch);
+
+        isRunning = false;
+    }
+
+    private Vector3 AddNoise(Vector3 vect)
+    {
+        vect += new Vector3(Random.Range(-xNoise, xNoise),
+            Random.Range(-yNoise, yNoise),
+            Random.Range(-zNoise, zNoise));
+
+        return vect;
+    }
 }
